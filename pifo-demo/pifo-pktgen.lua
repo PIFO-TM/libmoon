6cd094cb703e2db21c14ea1634c6ed274156f6b5
Stephen Ibanez <sibanez@g.hmc.edu>
2018-06-04 13:16:22 -0700

    Updated the pifo-pktgen.lua script to accept number of packets to send

--- pifo-demo/pifo-pktgen.lua
+++ pifo-demo/pifo-pktgen.lua
@@ -1,4 +1,4 @@
---- A simple UDP packet generator
+--- A simple TCP packet generator
 local lm     = require "libmoon"
 local device = require "device"
 local stats  = require "stats"
@@ -7,13 +7,15 @@ local memory = require "memory"
 
 -- set addresses here
 local DST_MAC       = "08:11:11:11:11:08"
-local PKT_LEN       = 1000
+local PKT_LEN       = 1496 --1020 --508 --60
 local SRC_IP        = "10.0.0.10"
 local DST_IP        = "10.1.0.10"
 local SRC_PORT_BASE = 0 -- actual port will be SRC_PORT_BASE + random(0, NUM_FLOWS-1)
 local DST_PORT      = 1234
-local NUM_FLOWS     = 4
-
+local NUM_FLOWS      = 4
+--local FLOW_PKT_CNT   = {[0] = 2, [1] = 1, [2] = 1, [3] = 1}
+local FLOW_PKT_CNT   = {[0] = 1, [1] = 1, [2] = 1, [3] = 1}
+local MAX_BURST_SIZE = 1
 
 -- the configure function is called on startup with a pre-initialized command line parser
 function configure(parser)
@@ -22,7 +24,8 @@ function configure(parser)
 	parser:option("-t --threads", "Number of threads per device."):args(1):convert(tonumber):default(1)
 	parser:option("-r --rate", "Transmit rate in Mbit/s per device."):args(1)
 	parser:option("-o --output", "File to output statistics to")
-	parser:option("-s --seconds", "Stop after n seconds")
+	parser:option("-s --seconds", "Stop after s seconds")
+	parser:option("-n --number", "Stop after n packets")
 	parser:flag("--csv", "Output in CSV format")
 	return parser:parse()
 end
@@ -42,7 +45,7 @@ function master(args,...)
 	device.waitForLinks()
 
 	-- print statistics
-	stats.startStatsTask{devices = args.dev, file = args.output, format = args.csv and "csv" or "plain"}
+--	stats.startStatsTask{devices = args.dev, file = args.output, format = args.csv and "csv" or "plain"}
 
 	-- configure tx rates and start transmit slaves
 	for i, dev in ipairs(args.dev) do
@@ -51,7 +54,7 @@ function master(args,...)
 			if args.rate then
 				queue:setRate(args.rate / args.threads)
 			end
-			lm.startTask("txSlave", queue, DST_MAC)
+			lm.startTask("txSlave", queue, DST_MAC, tonumber(args.number))
 		end
 	end
 
@@ -62,7 +65,7 @@ function master(args,...)
 	lm.waitForTasks()
 end
 
-function txSlave(queue, dstMac)
+function txSlave(queue, dstMac, num_pkts)
 	-- memory pool with default values for all packets, this is our archetype
 	local mempool = memory.createMemPool(function(buf)
 		buf:getTcpPacket():fill{
@@ -76,20 +79,40 @@ function txSlave(queue, dstMac)
 			pktLength = PKT_LEN
 		}
 	end)
+        local pkts_remaining = num_pkts
+        local done = false
+	local count = 0
+	local flowID = 0
 	-- a bufArray is just a list of buffers from a mempool that is processed as a single batch
 	local bufs = mempool:bufArray()
-	while lm.running() do -- check if Ctrl+c was pressed
+	while lm.running() and done == false do -- check if Ctrl+c was pressed
 		-- this actually allocates some buffers from the mempool the array is associated with
 		-- this has to be repeated for each send because sending is asynchronous, we cannot reuse the old buffers here
-		bufs:alloc(PKT_LEN)
+		if num_pkts==nil then
+			bufs:allocN(PKT_LEN, MAX_BURST_SIZE)
+		elseif pkts_remaining > MAX_BURST_SIZE then
+			bufs:allocN(PKT_LEN, MAX_BURST_SIZE)
+			pkts_remaining = pkts_remaining - MAX_BURST_SIZE
+		else
+			bufs:allocN(PKT_LEN, pkts_remaining)
+			done = true
+		end
+
 		for i, buf in ipairs(bufs) do
 			-- packet framework allows simple access to fields in complex protocol stacks
 			local pkt = buf:getTcpPacket()
-			pkt.tcp:setSrcPort(SRC_PORT_BASE + (i % NUM_FLOWS))
+			if count < FLOW_PKT_CNT[flowID] then
+				count = count + 1
+			else
+				count = 0
+				flowID = (flowID + 1) % NUM_FLOWS
+			end
+			pkt.tcp:setSrcPort(SRC_PORT_BASE + flowID)
 		end
---		bufs:offloadTcpChecksums()
+		bufs:offloadTcpChecksums()
 		-- send out all packets and frees old bufs that have been sent
 		queue:send(bufs)
 	end
+	log:info("txSlave finished!")
 end
 
